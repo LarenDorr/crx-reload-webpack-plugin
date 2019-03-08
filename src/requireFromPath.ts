db21672d2c11d17f83c1fe6a1ae0e87b6d0b8d4e
larendorr <larendorrx@gmail.com>
2019-03-08 09:22:55 +0800

    :bug: (src) fix memory leak
    after delete require cache, but it's parent still have reference
    
    Closes #1

--- src/requireFromPath.ts
+++ src/requireFromPath.ts
@@ -2,11 +2,19 @@ import logger from './logger'
 
 export default function requireFromPath(filePath: string): Manifest{
 	let obj: Manifest
+	debugger
 	try {
 		let nativeRequire = __non_webpack_require__ // us node native require
 		obj = nativeRequire(filePath)
 		logger.info(`required ${filePath}.`)
+		// fix memory leak
+		let parent = nativeRequire.cache[filePath].parent
+		let index = parent.children.indexOf(nativeRequire.cache[filePath])
+		if (index >= 0) {
+			parent.children.splice(index, 1)			
+		}
 		delete nativeRequire.cache[filePath]
+		
 		logger.info(`deleted required manifest cache`)
 	} catch (error) {
 		logger.error(error)
